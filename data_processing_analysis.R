bd2c3c6d280e1d486e2e4764f90e77cea8acdb09
Bret Beheim <bret_beheim@eva.mpg.de>
2021-08-26 15:01:06 +0200

    renamed R script and removed some redundant code

--- data_processing_analysis.R
+++ data_processing_analysis.R
@@ -3,9 +3,7 @@ setwd("/Users/bretbeheim/Dropbox/projects/2021/swc-spring-school/cardio-multisit
 
 library(readxl)
 
-# dat <- read_xlsx("Data_Final_092315.xlsx")
-# dat <- read_xlsx("Data_Final_092315 copy.xlsx")
-dat <- read_xlsx("Data_Final_092315 BAB.xlsx") # USE THIS ONE
+dat <- read_xlsx("Data_Final_092315 BAB.xlsx")
 
 dat$CoronaryCA <- as.numeric(dat$CoronaryCA)
 dat$Age_Part <- as.numeric(dat$Age_Part)
@@ -25,17 +23,9 @@ load("data_clean.RData")
 
 nrow(dat) # 16 observations
 
-# summary(dat$Sex)
-
-# summary(dat$Age_Part)
 # mean is 60.00 years old
 
-# m0 <- lm(ln_cac_plus1 ~ Age_Part, data = dat)
 m1 <- lm(ln_cac_plus1 ~ Age_Part + Sex, data = dat)
-
-# png("analysisplot.png", res = 300, height = 5, width = 5, units = "in")
-# plot(dat$Age_Part, dat$ln_cac_plus1, pch = ifelse(dat$Sex == 1, 1, 20))
-# dev.off()
 
 png("analysisplot_new.png", res = 300, height = 5, width = 5, units = "in")
 plot(dat$Age_Part, dat$ln_cac_plus1, pch = ifelse(dat$Sex == 1, 1, 20))
@@ -43,6 +33,6 @@ curve(coef(m1)[1] + coef(m1)[2] * x, add = TRUE, col = "gray")
 curve(coef(m1)[1] + coef(m1)[2] * x + coef(m1)[3], add = TRUE)
 dev.off()
 
-# png("boxplot.png", res = 300, height = 5, width = 5, units = "in")
-# boxplot(dat$ln_cac_plus1 ~ dat$male)
-# dev.off()
+png("boxplot.png", res = 300, height = 5, width = 5, units = "in")
+boxplot(dat$ln_cac_plus1 ~ dat$male)
+dev.off()
